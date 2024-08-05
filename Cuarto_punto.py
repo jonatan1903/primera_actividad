273c182da866eda26745971706acf590fbcc88ce
Jonathan <jonatannorenagrisales@gmail.com>
2024-08-04 21:43:48 -0500

    hice el Cuarto_punto

---
+++ Cuarto_punto.py
@@ -0,0 +1,9 @@
+num = int(input("Ingrese un numero para determinar si es par o impar: "))
+
+if (num % 2 == 0):
+
+    print("El numero ", num , "es par")
+    
+else:
+    
+    print("El numero ", num , "es impar")
